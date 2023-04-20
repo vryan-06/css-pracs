4659047b736e11dd0a5f30dc64a32a91655c9cc1
V Ryan <ryan19vt@gmail.com>
2023-04-20 10:50:18 +0530

    updated

--- project/add_watermark.py
+++ project/add_watermark.py
@@ -9,15 +9,9 @@ watermark_image = cv2.imread('watermark.png')
 watermark_image = cv2.resize(watermark_image, (original_image.shape[1], original_image.shape[0]))
 watermark_image_gray = cv2.cvtColor(watermark_image, cv2.COLOR_BGR2GRAY)
 
-ret, watermark_mask = cv2.threshold(watermark_image_gray, 0, 255, cv2.THRESH_BINARY)
-watermark_mask_inv = cv2.bitwise_not(watermark_mask)
-
-watermark_mask = cv2.cvtColor(watermark_mask, cv2.COLOR_GRAY2BGR)
-watermark_mask_inv = cv2.cvtColor(watermark_mask_inv, cv2.COLOR_GRAY2BGR)
-
 alpha = 0.3
 watermarked_image = cv2.addWeighted(original_image, 1 - alpha, watermark_image, alpha, 0)
-cv2.imwrite('watermarked_image.png', watermarked_image)
+cv2.imwrite('watermarked_image2.png', watermarked_image)
 
 resized_image = cv2.resize(watermarked_image, (600, 400))
 cv2.imshow('Resized Image', resized_image)
