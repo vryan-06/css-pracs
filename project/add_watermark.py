e02fb3a529bd2802f6fff2e2b62951a773def60b
V Ryan <ryan19vt@gmail.com>
2023-04-21 01:06:03 +0530

    updated code

--- project/add_watermark.py
+++ project/add_watermark.py
@@ -7,7 +7,6 @@ watermark_image = cv2.imread('watermark.png')
 # print(watermark_image.shape)
 
 watermark_image = cv2.resize(watermark_image, (original_image.shape[1], original_image.shape[0]))
-watermark_image_gray = cv2.cvtColor(watermark_image, cv2.COLOR_BGR2GRAY)
 
 alpha = 0.3
 watermarked_image = cv2.addWeighted(original_image, 1 - alpha, watermark_image, alpha, 0)
